3bc6953efe1f5bcf6210b1a530734fbc1ea188e0
Atroopa <atroopa@gmail.com>
2022-12-02 20:09:07 +0330

    navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+       <h1 className='w-full text-right text-3xl font-bold text-[#00df9a]' >املاک دهکده </h1>
+      <ul>
+        <li> خانه </li>
+        <li> موسسه </li>
+        <li> منابع </li>
+        <li> درباره ما </li>
+        <li> آدرس ما  </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
+
+
+
+
